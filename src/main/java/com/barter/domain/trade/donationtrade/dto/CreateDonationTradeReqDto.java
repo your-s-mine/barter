247c0facd6ebaa0b1ad82567ced8b92fb93b25ea
kimwanyoung <dhks2869@gmail.com>
2024-12-04 17:09:39 +0900

    refactor : 생성 dto 검증 메세지 추가

--- src/main/java/com/barter/domain/trade/donationtrade/dto/CreateDonationTradeReqDto.java
+++ src/main/java/com/barter/domain/trade/donationtrade/dto/CreateDonationTradeReqDto.java
@@ -2,6 +2,7 @@ package com.barter.domain.trade.donationtrade.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -9,12 +10,13 @@ import lombok.Getter;
 @Getter
 public class CreateDonationTradeReqDto {
 	private Long productId;
+	@Min(value = 1, message = "최대 수량은 한 개 이상만 가능합니다.")
 	private Integer maxAmount;
 	@NotBlank
-	@Size(min = 5)
+	@Size(min = 5, message = "제목은 5글자 이상만 가능합니다.")
 	private String title;
 	@NotBlank
-	@Size(min = 5)
+	@Size(min = 5, message = "설명은 5글자 이상만 가능합니다.")
 	private String description;
 	private LocalDateTime endedAt;
 }
