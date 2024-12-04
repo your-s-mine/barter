053a19bf6f80a2a0e8bdbc06c03bb7b4627e4b77
hemgom <reborn.april23@gmail.com>
2024-12-04 20:37:32 +0900

    [#28] feat: RegisteredProductController.findRegisteredProducts() 작성

--- src/main/java/com/barter/domain/product/controller/RegisteredProductController.java
+++ src/main/java/com/barter/domain/product/controller/RegisteredProductController.java
@@ -1,11 +1,15 @@
 package com.barter.domain.product.controller;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +43,19 @@ public class RegisteredProductController {
 		// 구현 이후 해당 메서드의 파라미터로 요청 회원 정보를 전달받아 활용하는 쪽으로 수정할 계획입니다.
 
 		return registeredProductService.findRegisteredProduct(registeredProductId);
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public PagedModel<FindRegisteredProductResDto> findRegisteredProducts(
+		@RequestParam(name = "pageNum", defaultValue = "1") int pageNum,
+		@RequestParam(name = "pageSize", defaultValue = "10") int pageSize) {
+		// 인증/인가 파트 구현이 끝난다면, 'REGISTERED_PRODUCTS' 테이블에서 요청 회원이 생성한 등록 물품들을 조회하도록 할 것 같습니다.
+
+		PageRequest pageRequest = PageRequest.of(
+			pageNum - 1, pageSize, Sort.Direction.DESC, "createdAt"
+		);
+
+		return registeredProductService.findRegisteredProducts(pageRequest);
 	}
 }
