4595a476c91d08a3045dcac7e98f459566b41f43
hemgom <reborn.april23@gmail.com>
2024-12-04 17:14:27 +0900

    [#15] fix: SuggestedProduct 클래스 필드 image -> images 로 수정

--- src/main/java/com/barter/domain/product/entity/SuggestedProduct.java
+++ src/main/java/com/barter/domain/product/entity/SuggestedProduct.java
@@ -2,14 +2,17 @@ package com.barter.domain.product.entity;
 
 import com.barter.domain.BaseTimeStampEntity;
 import com.barter.domain.member.entity.Member;
+import com.barter.domain.product.dto.request.CreateSuggestedProductReqDto;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,7 +27,24 @@ public class SuggestedProduct extends BaseTimeStampEntity {
 	private Long id;
 	private String name;
 	private String description;
-	private String image;
-	@ManyToOne
+	private String images;
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Member member;
+
+	@Builder
+	public SuggestedProduct(String name, String description, String images, Member member) {
+		this.name = name;
+		this.description = description;
+		this.images = images;
+		this.member = member;
+	}
+
+	public static SuggestedProduct create(CreateSuggestedProductReqDto request, Member member) {
+		return SuggestedProduct.builder()
+			.name(request.getName())
+			.description(request.getDescription())
+			.images(request.getImages())
+			.member(member)
+			.build();
+	}
 }
