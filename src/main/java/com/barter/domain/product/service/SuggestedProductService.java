1b22b57861d9f9d98e6da45b29590d3c08c54bb2
hemgom <reborn.april23@gmail.com>
2024-12-05 17:03:38 +0900

    [#58] feat: SuggestedProductService.findSuggestedProducts() 작성

--- src/main/java/com/barter/domain/product/service/SuggestedProductService.java
+++ src/main/java/com/barter/domain/product/service/SuggestedProductService.java
@@ -1,5 +1,8 @@
 package com.barter.domain.product.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
 
 import com.barter.domain.member.entity.Member;
@@ -33,5 +36,13 @@ public class SuggestedProductService {
 			.orElseThrow(() -> new IllegalArgumentException("Suggested product not found"));
 
 		return FindSuggestedProductResDto.from(foundProduct);
+	}
+
+	// 인증/인가 구현되면 요청 회원이 생성한 '등록 물품들만' 조회하도록 수정할 것으로 보입니다.
+	public PagedModel<FindSuggestedProductResDto> findSuggestedProducts(Pageable pageable) {
+		Page<FindSuggestedProductResDto> foundProducts = suggestedProductRepository.findAll(pageable)
+			.map(FindSuggestedProductResDto::from);
+
+		return new PagedModel<>(foundProducts);
 	}
 }
