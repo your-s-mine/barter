d9fb5e47172993de81832aba14f99f97b738c6ac
tesbro1194 <tes1194@naver.com>
2024-12-05 10:50:07 +0900

    feat: 즉시 교환 삭제
    [#31]

--- src/main/java/com/barter/domain/trade/immediatetrade/controller/ImmediateTradeController.java
+++ src/main/java/com/barter/domain/trade/immediatetrade/controller/ImmediateTradeController.java
@@ -2,6 +2,7 @@ package com.barter.domain.trade.immediatetrade.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,5 +40,10 @@ public class ImmediateTradeController {
 	public ResponseEntity<FindImmediateTradeResDto> update(@PathVariable Long tradeId,
 		@RequestBody @Valid UpdateImmediateTradeReqDto reqDto) throws IllegalAccessException {
 		return new ResponseEntity<>(immediateTradeService.update(tradeId, reqDto), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{tradeId}")
+	public ResponseEntity<String> delete(@PathVariable Long tradeId) {
+		return new ResponseEntity<>(immediateTradeService.delete(tradeId), HttpStatus.OK);
 	}
 }
