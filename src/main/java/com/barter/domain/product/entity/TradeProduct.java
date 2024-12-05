2373c5ebf100c45319cf4d220f040e048567ada3
wootaepark <pakdal99@gmail.com>
2024-12-05 12:49:21 +0900

    refactor : TradeProduct 빌더 생성자 구현

--- src/main/java/com/barter/domain/product/entity/TradeProduct.java
+++ src/main/java/com/barter/domain/product/entity/TradeProduct.java
@@ -8,8 +8,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +27,23 @@ public class TradeProduct {
 	private Long tradeId;
 	@Enumerated(EnumType.STRING)
 	private TradeType tradeType;
+
+	@ManyToOne
+	private RegisteredProduct registeredProduct;
+
+	@Builder
+	protected TradeProduct(Long tradeId, TradeType tradeType, RegisteredProduct registeredProduct) {
+		this.tradeId = tradeId;
+		this.tradeType = tradeType;
+		this.registeredProduct = registeredProduct;
+	}
+
+	public static TradeProduct createTradeProduct(Long tradeId, TradeType tradeType,
+		RegisteredProduct registeredProduct) {
+		return TradeProduct.builder()
+			.tradeId(tradeId)
+			.tradeType(tradeType)
+			.registeredProduct(registeredProduct)
+			.build();
+	}
 }
