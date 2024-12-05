97cadbdd1eec26e567b2af70eb10fe53e24b83b1
hemgom <reborn.april23@gmail.com>
2024-12-05 17:42:14 +0900

    [#59] feat: UpdateSuggestedProductInfoReqDto 생성

---
+++ src/main/java/com/barter/domain/product/dto/request/UpdateSuggestedProductInfoReqDto.java
@@ -0,0 +1,27 @@
+package com.barter.domain.product.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UpdateSuggestedProductInfoReqDto {
+
+	@NotNull
+	private Long id;
+
+	@NotNull
+	@Size(min = 5)
+	private String name;
+
+	@NotNull
+	@Size(min = 5)
+	private String description;
+
+	@NotNull
+	private String images;
+
+	// 인증/인가 전 테스트를 위해 RequestBody 에 요청 회원의 ID 를 전달받도록 함, 이후 삭제 예정
+	@NotNull
+	private Long memberId;
+}
