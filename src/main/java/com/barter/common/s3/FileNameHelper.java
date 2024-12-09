251e6f2568c87d4307c169215271230ef9c9aab9
kimwanyoung <dhks2869@gmail.com>
2024-12-09 19:08:16 +0900

    [#97] feat : S3에 이미지 업로드

---
+++ src/main/java/com/barter/common/s3/FileNameHelper.java
@@ -0,0 +1,21 @@
+package com.barter.common.s3;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class FileNameHelper {
+
+	public static String createFileName(String fileName) {
+		return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+	}
+
+	private static String getFileExtension(String fileName) {
+		try {
+			return fileName.substring(fileName.lastIndexOf("."));
+		} catch (StringIndexOutOfBoundsException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+		}
+	}
+}
