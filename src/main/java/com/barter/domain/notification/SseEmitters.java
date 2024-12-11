2f3e31152a163e546b88f43443e429c9266892ed
hemgom <reborn.april23@gmail.com>
2024-12-11 20:04:46 +0900

    [#116] feat: SseEmitters 클래스 수정

--- src/main/java/com/barter/domain/notification/SseEmitters.java
+++ src/main/java/com/barter/domain/notification/SseEmitters.java
@@ -1,35 +1,31 @@
-package com.barter.domain.notification.respository;
+package com.barter.domain.notification;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Repository
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
 public class SseEmitters {
 
 	private final Map<Long, SseEmitter> emitterMap = new ConcurrentHashMap<>();
 
-	@Override
 	public SseEmitter saveEmitter(Long emitterKey, SseEmitter emitter) {
 		emitterMap.put(emitterKey, emitter);
 		return emitter;
 	}
 
-	@Override
-	public SseEmitter findByKey(Long emitterKey) {
-		return emitterMap.get(emitterKey);
-	}
-
-	@Override
 	public void deleteEmitter(Long emitterKey) {
 		emitterMap.remove(emitterKey);
 	}
 
-	private void sendEvent(Long memberId, String eventName, Object data) {
-		SseEmitter emitter = sseEmitterRepository.findByKey(memberId);
+	public void sendEvent(Long memberId, String eventName, Object data) {
+		SseEmitter emitter = emitterMap.get(memberId);
 		if (emitter == null) {
 			// 전달 대상 사용자의 SseEmitter 가 없다면 접속하지 않은 사용자이므로 이벤트 전달 중지
 			log.info("전달 대상 사용자 접속하지 않음");
