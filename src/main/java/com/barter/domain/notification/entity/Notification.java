89061b9041f24f690d9853e2b1469c9c94ef2718
hemgom <reborn.april23@gmail.com>
2024-12-11 20:05:41 +0900

    [#116] feat: Notification 클래스 수정

--- src/main/java/com/barter/domain/notification/entity/Notification.java
+++ src/main/java/com/barter/domain/notification/entity/Notification.java
@@ -1,8 +1,11 @@
 package com.barter.domain.notification.entity;
 
 import com.barter.domain.BaseTimeStampEntity;
+import com.barter.domain.product.enums.TradeType;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,29 +18,33 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "ACTIVITY_NOTIFICATIONS")
-public class ActivityNotification extends BaseTimeStampEntity {  // Notification 으로 변경
+@Table(name = "NOTIFICATIONS")
+public class Notification extends BaseTimeStampEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String message;
-	private String url;
+	@Enumerated(EnumType.STRING)
+	private TradeType tradeType;
+	private Long tradeId;     // 연관관계 매핑, DB 연관관계 없는거로 생각했습니다.
 	private boolean isRead;
 	private Long memberId;    // 연관관계 매핑은 하지 않되, DB 연관관계(FK)는 맺는거로 생각했습니다.
 
 	@Builder
-	public ActivityNotification(String message, String url, boolean isRead, Long memberId) {
+	public Notification(String message, TradeType tradeType, Long tradeId, boolean isRead, Long memberId) {
 		this.message = message;
-		this.url = url;
+		this.tradeType = tradeType;
+		this.tradeId = tradeId;
 		this.isRead = isRead;
 		this.memberId = memberId;
 	}
 
-	public static ActivityNotification create(String message, String url, Long memberId) {
-		return ActivityNotification.builder()
+	public static Notification create(String message, TradeType tradeType, Long tradeId, Long memberId) {
+		return Notification.builder()
 			.message(message)
-			.url(url)
+			.tradeType(tradeType)
+			.tradeId(tradeId)
 			.isRead(false)
 			.memberId(memberId)
 			.build();
