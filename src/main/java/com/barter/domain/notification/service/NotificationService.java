749472cb5afbb0ef3894181a30bdc47004d260b5
hemgom <reborn.april23@gmail.com>
2024-12-11 17:08:20 +0900

    [#116] feat: NotificationService 생성 및 subscribe(), sendEvent() 작성

--- src/main/java/com/barter/domain/notification/service/NotificationService.java
+++ src/main/java/com/barter/domain/notification/service/NotificationService.java
@@ -1,14 +1,82 @@
 package com.barter.domain.notification.service;
 
-import org.springframework.stereotype.Service;
+import static com.barter.domain.notification.enums.EventKind.*;
 
-import com.barter.domain.notification.respository.NotificationRepository;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.barter.domain.notification.respository.ActivityNotificationRepository;
+import com.barter.domain.notification.respository.SseEmitterRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
-	private final NotificationRepository notificationRepository;
+	private final ActivityNotificationRepository activityNotificationRepository;
+	private final SseEmitterRepository sseEmitterRepository;
+	private static final long TIMEOUT = 60 * 60 * 1000;
+
+	public SseEmitter subscribe(Long memberId) {
+		SseEmitter emitter = sseEmitterRepository.saveEmitter(memberId, new SseEmitter(TIMEOUT));
+
+		emitter.onTimeout(() -> {
+			emitter.complete();
+			log.info("타임아웃으로 SseEmitter 연결해제, emitterKey={}", memberId);
+		});
+
+		emitter.onError(error -> {
+			emitter.complete();
+			log.info("클라이언트 연결해제로 인해 SseEmitter 연결해제, emitterKey={}", memberId);
+		});
+
+		emitter.onCompletion(() -> {
+			sseEmitterRepository.deleteEmitter(memberId);
+			log.info("연결해제된 SseEmitter 를 Local-Memory 에서 삭제, emitterKey={}", memberId);
+		});
+
+		// 더미 이벤트가 없는 상태로 Timeout 발생시 예외가 발생하기에 SseEmitter 발급시 더미 이벤트를 전달합니다.
+		sendEvent(memberId, DEFAULT.getName(), DEFAULT.getMessage());
+
+		return emitter;
+	}
+
+	/* 메서드명 거슬림 좀 더 생각해볼 필요가 있음, 사용 예시(추후 삭제 예정)
+	     - 아래의 예시를 이벤트가 발생하는 비즈니스 로직에서 호출하면 됩니다.
+	     - 물론 해당 메서드를 호출할 서비스에 NotificationService 를 주입해야 합니다.
+	public void sendSuggestedProductStatusEvent(Long memberId, Long productId, SuggestedStatus status) {
+		ActivityNotification createdNotification = ActivityNotification.create(
+			"제안물품 상태가 " + status + "로 변경되었습니다.",
+			"/suggested-products/" + productId, memberId
+		);
+		ActivityNotification savedNotification = activityNotificationRepository.save(createdNotification);
+
+		// 엔티티 파라미터로 사용할게 아니라 DTO 에 담아 전달할 것(필요한 정보만)
+		sendEvent(memberId, "제안 물품 상태 변경", savedNotification);
+	}
+	*/
+
+	private void sendEvent(Long memberId, String eventName, Object data) {
+		SseEmitter emitter = sseEmitterRepository.findByKey(memberId);
+		if (emitter == null) {
+			// 전달 대상 사용자의 SseEmitter 가 없다면 접속하지 않은 사용자이므로 이벤트 전달 중지
+			log.info("전달 대상 사용자 접속하지 않음");
+			return;
+		}
+
+		try {
+			emitter.send(SseEmitter.event()
+				.id(String.valueOf(memberId))
+				.name(eventName)
+				.data(data)
+			);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
