491419d81fb05bfe829ddadc5c5a61e37cdc3254
hemgom <reborn.april23@gmail.com>
2024-12-12 12:08:46 +0900

    [#127] feat: NotificationController.deleteNotification() 작성

--- src/main/java/com/barter/domain/notification/controller/NotificationController.java
+++ src/main/java/com/barter/domain/notification/controller/NotificationController.java
@@ -6,16 +6,20 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import com.barter.domain.notification.dto.request.DeleteNotificationReqDto;
 import com.barter.domain.notification.dto.response.FindNotificationResDto;
 import com.barter.domain.notification.service.NotificationService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -44,5 +48,13 @@ public class NotificationController {
 		// 인증/인가 파트 구현이 끝난다면, 'NOTIFICATIONS' 테이블에서 요청 회원의 활동 알림들을 조회하도록 할 것 같습니다.
 
 		return notificationService.findActivityNotifications(pageable);
+	}
+
+	// 인증/인가 적용시 RequestBody 를 전달 받지 않고 HttpServletRequest 에서 검증 회원정보를 전달 받을 생각입니다.
+	// 알림 ID 의 경우 path parameter 를 통해 전달 받을 생각입니다.
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteNotification(@RequestBody @Valid DeleteNotificationReqDto request) {
+		notificationService.deleteNotification(request);
 	}
 }
