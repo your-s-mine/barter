952b1a4f3098145bdc561ecc0cc3fa1634fe6c89
kimwanyoung <dhks2869@gmail.com>
2024-12-12 10:50:23 +0900

    refactor : param 제거

--- src/main/java/com/barter/domain/member/repository/FavoriteKeywordRepository.java
+++ src/main/java/com/barter/domain/member/repository/FavoriteKeywordRepository.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.barter.domain.member.entity.FavoriteKeyword;
 
 public interface FavoriteKeywordRepository extends JpaRepository<FavoriteKeyword, Long> {
 	Optional<FavoriteKeyword> findByKeyword(String keyword);
 
-	List<FavoriteKeyword> findByKeywordIn(@Param("keyword") List<String> keywords);
+	List<FavoriteKeyword> findByKeywordIn(List<String> keywords);
 }
