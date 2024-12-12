58bd34466f2db23a09bd1801fe7fd272e1aa04c0
tesbro1194 <tes1194@naver.com>
2024-12-12 17:27:46 +0900

    chore : 테스트를 위한 생성자 및 빌더 어노테이션 추가
    [#151]

--- src/main/java/com/barter/domain/trade/immediatetrade/dto/request/CreateImmediateTradeReqDto.java
+++ src/main/java/com/barter/domain/trade/immediatetrade/dto/request/CreateImmediateTradeReqDto.java
@@ -4,9 +4,11 @@ import com.barter.domain.product.entity.RegisteredProduct;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateImmediateTradeReqDto {
 	RegisteredProduct registeredProduct;
 	@NotBlank
