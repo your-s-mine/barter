1ffa8cf6790094229daf84d5d830208ad888cda1
hemgom <reborn.april23@gmail.com>
2024-12-13 09:30:39 +0900

    [#173] refacotr: 알림 구독 기능 리팩토링

--- src/main/java/com/barter/domain/notification/controller/NotificationController.java
+++ src/main/java/com/barter/domain/notification/controller/NotificationController.java
@@ -9,15 +9,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import com.barter.domain.notification.dto.request.UpdateNotificationStatusReqDto;
+import com.barter.domain.auth.dto.VerifiedMember;
 import com.barter.domain.notification.dto.request.DeleteNotificationReqDto;
+import com.barter.domain.notification.dto.request.UpdateNotificationStatusReqDto;
 import com.barter.domain.notification.dto.response.FindNotificationResDto;
 import com.barter.domain.notification.service.NotificationService;
 
@@ -31,15 +31,10 @@ public class NotificationController {
 
 	private final NotificationService notificationService;
 
-	@GetMapping(value = "/subscribe/{memberId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public SseEmitter subscribe(
-		@PathVariable(name = "memberId") Long memberId
-	) {
-		// 현재 인증/인가가 구현되지 않아 테스트를 위해 'path parameter' 로 요청 회원 ID 를 전달받고 있습니다.
-		// 이 부분은 추후 인증/인가가 구현되면 수정할 계획입니다.
-
-		return notificationService.subscribe(memberId);
+	public SseEmitter subscribe(VerifiedMember verifiedMember) {
+		return notificationService.subscribe(verifiedMember.getId());
 	}
 
 	@GetMapping("/activity")
