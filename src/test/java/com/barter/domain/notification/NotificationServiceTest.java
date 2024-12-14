1d6b017f153eb034c419e7bf86900d771129e4ed
hemgom <reborn.april23@gmail.com>
2024-12-15 01:43:51 +0900

    [#214] test: 키워드 알림 다건 조회 - 성공 테스트 작성

---
+++ src/test/java/com/barter/domain/notification/NotificationServiceTest.java
@@ -0,0 +1,96 @@
+package com.barter.domain.notification;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedModel;
+
+import com.barter.domain.notification.dto.response.FindNotificationResDto;
+import com.barter.domain.notification.entity.Notification;
+import com.barter.domain.notification.enums.NotificationType;
+import com.barter.domain.notification.respository.NotificationRepository;
+import com.barter.domain.notification.service.NotificationService;
+import com.barter.domain.product.enums.TradeType;
+
+@ExtendWith(MockitoExtension.class)
+public class NotificationServiceTest {
+
+	@Mock
+	private NotificationRepository notificationRepository;
+	@Mock
+	private SseEmitters sseEmitters;
+
+	@InjectMocks
+	private NotificationService notificationService;
+
+	@Test
+	@DisplayName("키워드 알림 다건 조회 - 성공 테스트")
+	void findKeywordNotificationsTest_Success() {
+		//given
+		Long verifiedMemberId = 1L;
+		Pageable pageable = PageRequest.of(
+			0, 10, Sort.by(Sort.Direction.DESC, "createdAt")
+		);
+		Notification notification1 = Notification.builder()
+			.id(1L)
+			.message("test notification1")
+			.tradeType(TradeType.DONATION)
+			.tradeId(10L)
+			.notificationType(NotificationType.KEYWORD)
+			.isRead(false)
+			.memberId(1L)
+			.build();
+
+		Notification notification2 = Notification.builder()
+			.id(2L)
+			.message("test notification2")
+			.tradeType(TradeType.PERIOD)
+			.tradeId(14L)
+			.notificationType(NotificationType.KEYWORD)
+			.isRead(false)
+			.memberId(1L)
+			.build();
+
+		Notification notification3 = Notification.builder()
+			.id(3L)
+			.message("test notification3")
+			.tradeType(TradeType.IMMEDIATE)
+			.tradeId(25L)
+			.notificationType(NotificationType.KEYWORD)
+			.isRead(false)
+			.memberId(1L)
+			.build();
+
+		List<Notification> notifications = List.of(notification3, notification2, notification1);
+		Page<Notification> foundNotifications = new PageImpl<>(notifications, pageable, notifications.size());
+		when(notificationRepository.findAllKeywordNotification(pageable, verifiedMemberId))
+			.thenReturn(foundNotifications);
+
+		//when
+		PagedModel<FindNotificationResDto> response = notificationService.findKeywordNotifications(
+			pageable, verifiedMemberId
+		);
+
+		//then
+		assertThat(response).isNotNull();
+		assertThat(response.getContent()).hasSize(3);
+		assertThat(Objects.requireNonNull(response.getMetadata()).size()).isEqualTo(10);
+		assertThat(response.getMetadata().number()).isEqualTo(0);
+		assertThat(response.getMetadata().totalElements()).isEqualTo(3);
+		assertThat(response.getMetadata().totalPages()).isEqualTo(1);
+	}
+}
