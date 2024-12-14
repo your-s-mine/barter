22cada233fc8a50856556f17d40e6cd507f6449f
hemgom <reborn.april23@gmail.com>
2024-12-14 16:12:10 +0900

    [#197] test: RegisteredProductServiceTest.createRegisteredProductTest_Successful() 작성

---
+++ src/test/java/com/barter/domain/product/RegisteredProductServiceTest.java
@@ -0,0 +1,80 @@
+package com.barter.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.barter.common.s3.S3Service;
+import com.barter.domain.member.entity.Member;
+import com.barter.domain.product.dto.request.CreateRegisteredProductReqDto;
+import com.barter.domain.product.dto.response.CreateRegisteredProductResDto;
+import com.barter.domain.product.entity.RegisteredProduct;
+import com.barter.domain.product.enums.RegisteredStatus;
+import com.barter.domain.product.repository.RegisteredProductRepository;
+import com.barter.domain.product.service.RegisteredProductService;
+
+@ExtendWith(MockitoExtension.class)
+public class RegisteredProductServiceTest {
+
+	@Mock
+	private RegisteredProductRepository registeredProductRepository;
+	@Mock
+	private S3Service s3Service;
+
+	@InjectMocks
+	private RegisteredProductService registeredProductService;
+
+	@Test
+	@DisplayName("등록물품 생성 - 정상 작동 테스트")
+	public void createRegisteredProductTest_Successful() {
+		//given
+		CreateRegisteredProductReqDto request = CreateRegisteredProductReqDto.builder()
+			.name("test product")
+			.description("test description")
+			.build();
+
+		MultipartFile imageFile = new MockMultipartFile(
+			"file", "test.png", "text/plain", "test".getBytes(StandardCharsets.UTF_8)
+		);
+		List<MultipartFile> multipartFiles = List.of(imageFile, imageFile);
+
+		Long verifiedMemberId = 1L;
+
+		when(s3Service.uploadFile(multipartFiles)).thenReturn(List.of("testImage1", "testImage2"));
+
+		when(registeredProductRepository.save(any())).
+			thenReturn(RegisteredProduct.builder()
+				.id(1L)
+				.name(request.getName())
+				.description(request.getDescription())
+				.images(List.of("testImage1", "testImage2"))
+				.status(RegisteredStatus.PENDING)
+				.member(Member.builder().id(verifiedMemberId).build())
+				.build()
+			);
+
+		//when
+		CreateRegisteredProductResDto response = registeredProductService.createRegisteredProduct(
+			request, multipartFiles, verifiedMemberId
+		);
+
+		//then
+		assertThat(response).isNotNull();
+		assertThat(response.getName()).isEqualTo("test product");
+		assertThat(response.getDescription()).isEqualTo("test description");
+		assertThat(response.getImages().size()).isEqualTo(2);
+		assertThat(response.getStatus()).isEqualTo(RegisteredStatus.PENDING.name());
+		assertThat(response.getMemberId()).isEqualTo(verifiedMemberId);
+	}
+}
