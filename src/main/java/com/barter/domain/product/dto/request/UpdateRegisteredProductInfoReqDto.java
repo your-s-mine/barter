ae6ae9c787bea21dbedf9dd7ad39cb824fb5e7cb
hemgom <reborn.april23@gmail.com>
2024-12-14 20:32:34 +0900

    [#200] refactor: UpdateRegisteredProductInfoReqDto 테스트를 위해 @Builder 추가

--- src/main/java/com/barter/domain/product/dto/request/UpdateRegisteredProductInfoReqDto.java
+++ src/main/java/com/barter/domain/product/dto/request/UpdateRegisteredProductInfoReqDto.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UpdateRegisteredProductInfoReqDto {
 
 	@NotNull
