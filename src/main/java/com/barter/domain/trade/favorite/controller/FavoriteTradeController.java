8d69d128fbff8cb79d2cb0569f4a28c5f305f493
MIN GI SEO <syg9111@naver.com>
2024-12-14 12:02:39 +0900

    관섬 거래 삭제

--- src/main/java/com/barter/domain/trade/favorite/controller/FavoriteTradeController.java
+++ src/main/java/com/barter/domain/trade/favorite/controller/FavoriteTradeController.java
@@ -3,11 +3,7 @@ package com.barter.domain.trade.favorite.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.barter.domain.auth.dto.VerifiedMember;
 import com.barter.domain.trade.favorite.dto.CreateFavoriteTradeReqDto;
@@ -26,8 +22,8 @@ public class FavoriteTradeController {
 	// 관심 거래 생성
 	@PostMapping
 	public ResponseEntity<FavoriteTradeResDto> createFavoriteTrade(
-		VerifiedMember verifiedMember, // 인증 정보를 자동으로 주입
-		@RequestBody CreateFavoriteTradeReqDto requestDto) {
+			VerifiedMember verifiedMember, // 인증 정보를 자동으로 주입
+			@RequestBody CreateFavoriteTradeReqDto requestDto) {
 		// 서비스 호출
 		FavoriteTradeResDto responseDto = favoriteTradeService.createFavoriteTrade(verifiedMember, requestDto);
 		return ResponseEntity.ok(responseDto);
@@ -36,9 +32,19 @@ public class FavoriteTradeController {
 	// 관심 거래 조회
 	@GetMapping
 	public ResponseEntity<List<FavoriteTradeResDto>> findFavoriteTrades(
-		VerifiedMember verifiedMember) { // 인증 정보를 자동으로 주입
+			VerifiedMember verifiedMember) { // 인증 정보를 자동으로 주입
 		// 서비스 호출
 		List<FavoriteTradeResDto> favorites = favoriteTradeService.findFavoriteTrades(verifiedMember);
 		return ResponseEntity.ok(favorites);
+	}
+
+	// 관심 거래 삭제
+	@DeleteMapping("/{favoriteTradeId}")
+	public ResponseEntity<Void> deleteFavoriteTrade(
+			VerifiedMember verifiedMember,
+			@PathVariable Long favoriteTradeId) {
+		// 서비스 호출
+		favoriteTradeService.deleteFavoriteTrade(verifiedMember, favoriteTradeId);
+		return ResponseEntity.noContent().build();
 	}
 }
