9d8cd1aa3e3e81b650ea85eafd93acbece2498ed
hemgom <reborn.april23@gmail.com>
2024-12-15 16:43:08 +0900

    [#224] test: 제안 물품 다건 조회 - 정상 테스트 작성

---
+++ src/test/java/com/barter/domain/product/SuggestedProductServiceTest.java
@@ -0,0 +1,95 @@
+package com.barter.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedModel;
+
+import com.barter.common.s3.S3Service;
+import com.barter.domain.member.entity.Member;
+import com.barter.domain.product.dto.response.FindSuggestedProductResDto;
+import com.barter.domain.product.entity.SuggestedProduct;
+import com.barter.domain.product.enums.SuggestedStatus;
+import com.barter.domain.product.repository.SuggestedProductRepository;
+import com.barter.domain.product.service.SuggestedProductService;
+
+@ExtendWith(MockitoExtension.class)
+public class SuggestedProductServiceTest {
+
+	@Mock
+	private SuggestedProductRepository suggestedProductRepository;
+	@Mock
+	private S3Service s3Service;
+
+	@InjectMocks
+	private SuggestedProductService suggestedProductService;
+
+	@Test
+	@DisplayName("제안 물품 다건 조회 - 성공 테스트")
+	void findSuggestedProductsTest_Success() {
+		//given
+		Long verifiedMemberId = 1L;
+		Pageable pageable = PageRequest.of(
+			0, 10, Sort.by(Sort.Direction.DESC, "createdAt")
+		);
+
+		SuggestedProduct product1 = SuggestedProduct.builder()
+			.id(1L)
+			.name("test product1")
+			.description("product1 description")
+			.images(List.of("product1 image1", "product1 image2"))
+			.status(SuggestedStatus.PENDING)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		SuggestedProduct product2 = SuggestedProduct.builder()
+			.id(2L)
+			.name("test product2")
+			.description("product2 description")
+			.images(List.of("product2 image1"))
+			.status(SuggestedStatus.SUGGESTING)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		SuggestedProduct product3 = SuggestedProduct.builder()
+			.id(3L)
+			.name("test product3")
+			.description("product3 description")
+			.images(List.of("product3 image1", "product3 image2", "product3 image3"))
+			.status(SuggestedStatus.ACCEPTED)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		List<SuggestedProduct> products = List.of(product3, product2, product1);
+		Page<SuggestedProduct> foundProducts = new PageImpl<>(products, pageable, products.size());
+		when(suggestedProductRepository.findAllByMemberId(pageable, verifiedMemberId))
+			.thenReturn(foundProducts);
+
+		//when
+		PagedModel<FindSuggestedProductResDto> response = suggestedProductService.findSuggestedProducts(
+			pageable, verifiedMemberId
+		);
+
+		//then
+		assertThat(response).isNotNull();
+		assertThat(response.getContent()).hasSize(3);
+		assertThat(Objects.requireNonNull(response.getMetadata()).size()).isEqualTo(10);
+		assertThat(response.getMetadata().number()).isEqualTo(0);
+		assertThat(response.getMetadata().totalElements()).isEqualTo(3);
+		assertThat(response.getMetadata().totalPages()).isEqualTo(1);
+	}
+}
