dc39937690ad6dc52a3304b4298cf9f7c211410a
hemgom <reborn.april23@gmail.com>
2024-12-15 18:07:59 +0900

    [#227] test: 제안 물품 삭제 - 성공 테스트 작성

---
+++ src/test/java/com/barter/domain/product/SuggestedProductServiceTest.java
@@ -0,0 +1,58 @@
+package com.barter.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.barter.common.s3.S3Service;
+import com.barter.domain.member.entity.Member;
+import com.barter.domain.product.entity.SuggestedProduct;
+import com.barter.domain.product.enums.SuggestedStatus;
+import com.barter.domain.product.repository.SuggestedProductRepository;
+import com.barter.domain.product.service.SuggestedProductService;
+
+@ExtendWith(MockitoExtension.class)
+public class SuggestedProductServiceTest {
+
+	@Mock
+	private SuggestedProductRepository suggestedProductRepository;
+	@Mock
+	private S3Service s3Service;
+
+	@InjectMocks
+	private SuggestedProductService suggestedProductService;
+
+	@Test
+	@DisplayName("제안 물품 삭제 - 성공 테스트")
+	void deleteSuggestedProductTest_Success() {
+		//given
+		Long suggestedProductId = 1L;
+		Long verifiedMemberId = 1L;
+
+		SuggestedProduct testProduct = SuggestedProduct.builder()
+			.id(suggestedProductId)
+			.status(SuggestedStatus.PENDING)
+			.images(List.of("image1", "image2"))
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+		suggestedProductRepository.save(testProduct);
+
+		when(suggestedProductRepository.findById(suggestedProductId))
+			.thenReturn(Optional.of(testProduct));
+
+		//when
+		suggestedProductService.deleteSuggestedProduct(suggestedProductId, verifiedMemberId);
+
+		//then
+		assertThat(suggestedProductRepository.count()).isEqualTo(0);
+	}
+}
