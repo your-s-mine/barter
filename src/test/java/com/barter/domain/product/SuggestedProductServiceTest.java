6b5e0cb287473a5f6558a5ff9a4e0cdc25c6b6be
hemgom <reborn.april23@gmail.com>
2024-12-15 16:04:24 +0900

    [#223] test: 제안 물품 단건 조회 - 정상 테스트 작성

---
+++ src/test/java/com/barter/domain/product/SuggestedProductServiceTest.java
@@ -0,0 +1,66 @@
+package com.barter.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.barter.common.s3.S3Service;
+import com.barter.domain.member.entity.Member;
+import com.barter.domain.product.dto.response.FindSuggestedProductResDto;
+import com.barter.domain.product.entity.SuggestedProduct;
+import com.barter.domain.product.enums.SuggestedStatus;
+import com.barter.domain.product.repository.SuggestedProductRepository;
+import com.barter.domain.product.service.SuggestedProductService;
+
+@ExtendWith(MockitoExtension.class)
+public class SuggestedProductServiceTest {
+
+	@Mock
+	private SuggestedProductRepository suggestedProductRepository;
+	@Mock
+	private S3Service s3Service;
+
+	@InjectMocks
+	private SuggestedProductService suggestedProductService;
+
+	@Test
+	@DisplayName("제안 물품 단건 조회 - 성공 테스트")
+	void findSuggestedProductTest_Success() {
+		//given
+		Long suggestedProductId = 1L;
+		Long verifiedMemberId = 1L;
+
+		when(suggestedProductRepository.findById(suggestedProductId)).thenReturn(
+			Optional.of(SuggestedProduct.builder()
+				.id(1L)
+				.name("test product")
+				.description("test description")
+				.images(List.of("test image1", "test image2"))
+				.member(Member.builder().id(1L).build())
+				.status(SuggestedStatus.PENDING)
+				.build()
+			)
+		);
+
+		//when
+		FindSuggestedProductResDto response = suggestedProductService.findSuggestedProduct(
+			suggestedProductId, verifiedMemberId
+		);
+
+		//then
+		assertThat(response).isNotNull();
+		assertThat(response.getName()).isEqualTo("test product");
+		assertThat(response.getDescription()).isEqualTo("test description");
+		assertThat(response.getImages()).containsExactly("test image1", "test image2");
+		assertThat(response.getStatus()).isEqualTo(SuggestedStatus.PENDING.name());
+	}
+}
