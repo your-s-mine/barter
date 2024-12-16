ae9fa4df6e24e69fcd0d7e7c213434668ab788f8
kimwanyoung <dhks2869@gmail.com>
2024-12-16 17:51:44 +0900

    [#238] refactor : kakao oauth redirect시 요청되는 url interceptor exclude

--- src/main/java/com/barter/config/WebConfig.java
+++ src/main/java/com/barter/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(authInterceptor)
 			.addPathPatterns("/**")
-			.excludePathPatterns("/auth/**", "/oauth/**");
+			.excludePathPatterns("/auth/**", "/oauth/**", "/favicon.ico", "/error");
 	}
 
 	@Override
