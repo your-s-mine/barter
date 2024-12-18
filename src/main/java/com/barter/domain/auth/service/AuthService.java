6d1cdf25f1ae6c2e8a97746506727b1c30267585
kimwanyoung <dhks2869@gmail.com>
2024-12-18 14:33:13 +0900

    [#240] feat : 적용 예시를 위해 AuthException, MemberException 구현

--- src/main/java/com/barter/domain/auth/service/AuthService.java
+++ src/main/java/com/barter/domain/auth/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.barter.domain.auth.service;
 
+import static com.barter.exception.enums.ExceptionCode.*;
+
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -8,13 +10,13 @@ import org.springframework.util.StringUtils;
 import com.barter.domain.auth.dto.SignInReqDto;
 import com.barter.domain.auth.dto.SignInResDto;
 import com.barter.domain.auth.dto.SignUpReqDto;
-import com.barter.domain.auth.exception.DuplicateEmailException;
-import com.barter.domain.auth.exception.InvalidCredentialsException;
 import com.barter.domain.member.entity.Member;
 import com.barter.domain.member.repository.MemberRepository;
 import com.barter.domain.oauth.dto.LoginOAuthMemberDto;
 import com.barter.domain.oauth.dto.LoginOAuthMemberResDto;
 import com.barter.domain.oauth.enums.OAuthProvider;
+import com.barter.exception.customexceptions.AuthException;
+import com.barter.exception.customexceptions.MemberException;
 import com.barter.security.JwtUtil;
 import com.barter.security.PasswordEncoder;
 
@@ -34,7 +36,7 @@ public class AuthService {
 
 		// 이메일 중복 검증
 		if (memberRepository.existsByEmail(req.getEmail())) {
-			throw new DuplicateEmailException("중복된 이메일입니다.");
+			throw new AuthException(DUPLICATE_EMAIL);
 		}
 
 		// 비밀번호 암호화 후 저장
@@ -65,36 +67,36 @@ public class AuthService {
 
 	public SignInResDto signIn(SignInReqDto req) {
 		Member member = memberRepository.findByEmail(req.getEmail())
-				.orElseThrow(() -> new InvalidCredentialsException("존재하지 않는 사용자입니다."));
+			.orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
 		if (!passwordEncoder.matches(req.getPassword(), member.getPassword())) {
-			throw new InvalidCredentialsException("비밀번호가 일치하지 않습니다.");
+			throw new AuthException(INVALID_PASSWORD);
 		}
 		String token = jwtUtil.createToken(member.getId(), member.getEmail());
 		return SignInResDto.builder()
-				.accessToken(token)
-				.build();
+			.accessToken(token)
+			.build();
 	}
 
 	public void signupWithOAuth(OAuthProvider provider, LoginOAuthMemberDto memberInfo) {
 		UUID uuid = UUID.randomUUID();
 		Member socialMember = Member.builder()
-				.provider(provider)
-				.providerId(memberInfo.getId())
-				.email(memberInfo.getEmail())
-				.password(passwordEncoder.encode(uuid.toString()))
-				.nickname(memberInfo.getNickname())
-				.build();
+			.provider(provider)
+			.providerId(memberInfo.getId())
+			.email(memberInfo.getEmail())
+			.password(passwordEncoder.encode(uuid.toString()))
+			.nickname(memberInfo.getNickname())
+			.build();
 		memberRepository.save(socialMember);
 	}
 
 	public LoginOAuthMemberResDto signinWithOAuth(OAuthProvider provider, LoginOAuthMemberDto memberInfo) {
 		return memberRepository.findByProviderAndProviderId(provider, memberInfo.getId())
-				.map(member -> {
-					String accessToken = jwtUtil.createToken(member.getId(), member.getEmail());
-					return LoginOAuthMemberResDto.builder()
-							.accessToken(accessToken)
-							.build();
-				})
-				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 멤버입니다."));
+			.map(member -> {
+				String accessToken = jwtUtil.createToken(member.getId(), member.getEmail());
+				return LoginOAuthMemberResDto.builder()
+					.accessToken(accessToken)
+					.build();
+			})
+			.orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
 	}
 }
--- src/main/java/com/barter/exception/enums/ExceptionCode.java
+++ src/main/java/com/barter/exception/enums/ExceptionCode.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExceptionCode {
 	// 인증, 인가
-	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 멤버입니다.");
+	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 멤버입니다."),
+	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다."),
+	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다.");
 
 	private final HttpStatus code;
 	private final String message;
