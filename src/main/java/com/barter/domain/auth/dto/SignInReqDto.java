b66b30024e50c4724325880c10b0a967a4e69aa1
MIN GI SEO <syg9111@naver.com>
2024-12-18 17:23:44 +0900

    ãë로그인 테스트 코드

--- src/main/java/com/barter/domain/auth/dto/SignInReqDto.java
+++ src/main/java/com/barter/domain/auth/dto/SignInReqDto.java
@@ -2,9 +2,11 @@ package com.barter.domain.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class SignInReqDto {
 
 	@NotBlank(message = "이메일은 필수 입력값입니다.")
