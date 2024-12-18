7096b508040b387fea6df82338c71d2819b8f4e4
hemgom <reborn.april23@gmail.com>
2024-12-18 13:54:10 +0900

    [#267] test: 사용 가능한 등록 물품 다건 조회 - 성공 테스트 작성

--- src/test/java/com/barter/domain/product/RegisteredProductServiceTest.java
+++ src/test/java/com/barter/domain/product/RegisteredProductServiceTest.java
@@ -31,6 +31,7 @@ import com.barter.domain.product.dto.request.CreateRegisteredProductReqDto;
 import com.barter.domain.product.dto.request.UpdateRegisteredProductInfoReqDto;
 import com.barter.domain.product.dto.request.UpdateRegisteredProductStatusReqDto;
 import com.barter.domain.product.dto.response.CreateRegisteredProductResDto;
+import com.barter.domain.product.dto.response.FindAvailableRegisteredProductsResDto;
 import com.barter.domain.product.dto.response.FindRegisteredProductResDto;
 import com.barter.domain.product.dto.response.UpdateRegisteredProductInfoResDto;
 import com.barter.domain.product.dto.response.UpdateRegisteredProductStatusResDto;
@@ -591,4 +592,55 @@ public class RegisteredProductServiceTest {
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("PENDING 상태인 경우에만 등록 물품을 삭제할 수 있습니다.");
 	}
+
+	@Test
+	@DisplayName("사용 가능한 등록 물품 다건 조회 - 성공 테스트")
+	void findAvailableRegisteredProductsTest_Success() {
+		//given
+		Long verifiedMemberId = 1L;
+
+		RegisteredProduct product1 = RegisteredProduct.builder()
+			.id(1L)
+			.name("test product1")
+			.description("test product1 description")
+			.images(List.of("test product1 image1", "test product1 image2"))
+			.status(RegisteredStatus.PENDING)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		RegisteredProduct product2 = RegisteredProduct.builder()
+			.id(2L)
+			.name("test product2")
+			.description("test product2 description")
+			.images(List.of("test product2 image1", "test product2 image2"))
+			.status(RegisteredStatus.PENDING)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		RegisteredProduct product3 = RegisteredProduct.builder()
+			.id(3L)
+			.name("test product3")
+			.description("test product3 description")
+			.images(List.of("test product3 image1", "test product3 image2"))
+			.status(RegisteredStatus.REGISTERING)
+			.member(Member.builder().id(verifiedMemberId).build())
+			.build();
+
+		when(registeredProductRepository.findAllAvailableRegisteredProduct(verifiedMemberId)).thenReturn(
+			List.of(product1, product2)
+		);
+
+		//when
+		List<FindAvailableRegisteredProductsResDto> response = registeredProductService.findAvailableRegisteredProducts(
+			verifiedMemberId);
+
+		//then
+		assertThat(response).isNotNull();
+		assertThat(response).hasSize(2);
+		assertThat(response.get(0).getId()).isEqualTo(1L);
+		assertThat(response.get(0).getName()).isEqualTo("test product1");
+		assertThat(response.get(1).getId()).isEqualTo(2L);
+		assertThat(response.get(1).getName()).isEqualTo("test product2");
+	}
+
 }
