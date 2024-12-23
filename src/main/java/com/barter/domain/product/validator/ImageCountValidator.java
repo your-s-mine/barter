2d6e9fc551713128f05480479a9ace7fdcafda30
hemgom <reborn.april23@gmail.com>
2024-12-23 20:21:42 +0900

    [#314] refactor: 등록물품, 사용 가능한 이미지 개수 확인 시

--- src/main/java/com/barter/domain/product/validator/ImageCountValidator.java
+++ src/main/java/com/barter/domain/product/validator/ImageCountValidator.java
@@ -1,11 +1,14 @@
 package com.barter.domain.product.validator;
 
+import com.barter.exception.customexceptions.ProductException;
+import com.barter.exception.enums.ExceptionCode;
+
 // 요구사항 정의서 반영 등록물품은 1~3 개의 이미지를 가져야함
 public class ImageCountValidator {
 
 	public static void checkImageCount(int newImageCount) {
 		if (newImageCount > 3 || newImageCount < 1) {
-			throw new IllegalArgumentException("1 ~ 3개 사이의 이미지를 가져야 합니다.");
+			throw new ProductException(ExceptionCode.NOT_VALID_IMAGE_COUNT);
 		}
 	}
 
@@ -13,7 +16,7 @@ public class ImageCountValidator {
 		int resultCount = deletedImageCount + newImageCount;
 
 		if (resultCount > 3 || resultCount < 1) {
-			throw new IllegalArgumentException("1 ~ 3개 사이의 이미지를 가져야 합니다.");
+			throw new ProductException(ExceptionCode.NOT_VALID_IMAGE_COUNT);
 		}
 	}
 }
