8fe1023ac1dcf9c9ec880cd51b09ca28bb1f1271
tesbro1194 <tes1194@naver.com>
2024-12-23 15:11:16 +0900

    refactor: deleteHistoryOver24hours() 오류 시 retry 와 로그 추가
    #313

--- src/main/java/com/barter/BarterApplication.java
+++ src/main/java/com/barter/BarterApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
