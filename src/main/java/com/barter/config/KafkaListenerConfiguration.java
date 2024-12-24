ffc480903c4c25137288491c98175bff933a09ec
wootaepark <pakdal99@gmail.com>
2024-12-24 11:15:48 +0900

    config : kafka producer & listener(consumer) 설정

---
+++ src/main/java/com/barter/config/KafkaListenerConfiguration.java
@@ -0,0 +1,49 @@
+package com.barter.config;
+
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.barter.domain.chat.dto.request.ChatMessageReqDto;
+
+@EnableKafka
+@Configuration
+public class KafkaListenerConfiguration {
+
+	// KafkaListener 컨테이너 팩토리를 생성하는 Bean 메서드
+	@Bean
+	ConcurrentKafkaListenerContainerFactory<String, ChatMessageReqDto> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, ChatMessageReqDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+
+	// Kafka ConsumerFactory 를 생성하는 Bean 메서드
+	@Bean
+	public ConsumerFactory<String, ChatMessageReqDto> consumerFactory() {
+		JsonDeserializer<ChatMessageReqDto> deserializer = new JsonDeserializer<>();
+
+		deserializer.addTrustedPackages("*"); // 모든 패키지 신뢰
+
+		// Kafka Consumer 구성을 위한 설정 값들을 설정
+		Map<String, Object> consumerConfigurations;
+
+		consumerConfigurations = Map.of(
+			ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+			ConsumerConfig.GROUP_ID_CONFIG, "adopt",
+			ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+			ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer,
+			ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+		return new DefaultKafkaConsumerFactory<>(consumerConfigurations, new StringDeserializer(), deserializer);
+
+	}
+}
---
+++ src/main/java/com/barter/config/KafkaProducerConfiguration.java
@@ -0,0 +1,46 @@
+package com.barter.config;
+
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.barter.domain.chat.dto.request.ChatMessageReqDto;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfiguration {
+
+	// Kafka ProducerFactory 생성 Bean 메서드
+	@Bean
+	public ProducerFactory<String, ChatMessageReqDto> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigurations());
+	}
+
+	// Kafka Producer 구성을 위한 설정값들을 포함한 맵을 반환하는 메서드
+	@Bean
+	public Map<String, Object> producerConfigurations() {
+		Map<String, Object> producerConfigurations;
+		producerConfigurations = Map.of(
+			ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+			ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+			ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class
+		);
+
+		return producerConfigurations;
+	}
+
+	// KafkaTemplate 을 생성하는 Bean 메서드
+
+	@Bean
+	public KafkaTemplate<String, ChatMessageReqDto> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+}
