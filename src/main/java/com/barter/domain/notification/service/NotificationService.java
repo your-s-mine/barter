ab2f346f8166e934f2d717f79e99a861657b0d35
hemgom <reborn.april23@gmail.com>
2024-12-30 15:30:46 +0900

    [#371] refactor: member, favoriteKeyword 필드 지연로딩 적용

--- src/main/java/com/barter/domain/notification/service/NotificationService.java
+++ src/main/java/com/barter/domain/notification/service/NotificationService.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.web.PagedModel;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -31,6 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@EnableAsync
 @RequiredArgsConstructor
 public class NotificationService implements MessageListener {
 
@@ -100,6 +103,7 @@ public class NotificationService implements MessageListener {
 		publishEvent("activity", publishMessage);
 	}
 
+	@Async
 	public void saveKeywordNotification(
 		EventKind eventKind, List<Long> memberIds, TradeType tradeType, Long tradeId
 	) {
