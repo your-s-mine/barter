421266b267dc83a17041201c44355781044fb749
tesbro1194 <tes1194@naver.com>
2025-01-01 15:09:29 +0900

    refactor: @RequestBody 사용을 위해 PostMapping 으로 바꿈.
    #373

--- src/main/java/com/barter/domain/search/controller/SearchController.java
+++ src/main/java/com/barter/domain/search/controller/SearchController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class SearchController {
 	private final SearchService searchService;
 
-	@GetMapping("/trades")
+	@PostMapping("/trades")
 	public ResponseEntity<List<SearchTradeResDto>> findTrades(@RequestParam String word,
 		@RequestBody SearchTradeReqDto reqDto) {
 		if (word == null || word.isBlank()) {
