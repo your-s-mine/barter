63f52b48b6215e87fb11610a7e677608bee95df1
wootaepark <pakdal99@gmail.com>
2025-01-02 15:48:16 +0900

    feat : PeriodTrade조회수 캐싱 기능 추가

--- src/main/java/com/barter/domain/trade/periodtrade/repository/PeriodTradeRepository.java
+++ src/main/java/com/barter/domain/trade/periodtrade/repository/PeriodTradeRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,10 +19,11 @@ public interface PeriodTradeRepository extends JpaRepository<PeriodTrade, Long> 
 	)
 	List<PeriodTrade> findPeriodTradesWithProduct(@Param("keyword") String keyword, String address1);
 
-	// @Query("SELECT pt FROM PeriodTrade pt ORDER BY pt.updatedAt DESC LIMIT :pageSize OFFSET :offset")
-	// Slice<PeriodTrade> findPeriodTradeByUpdatedAt(@Param("pageSize") int pageSize, @Param("offset") int offset);
-
 	@Query("SELECT pt FROM PeriodTrade pt ORDER BY pt.updatedAt DESC")
 	Slice<PeriodTrade> findPeriodTradeByUpdatedAt(Pageable pageable);
+
+	@Modifying
+	@Query("UPDATE PeriodTrade pt SET pt.viewCount = pt.viewCount + :viewCount WHERE pt.id = :periodTradeId")
+	void updateViewCount(@Param("periodTradeId") Long periodTradeId, @Param("viewCount") Long viewCount);
 
 }
