636736d98aebc34ab78afc151d62b5e6b2d0df26
wootaepark <pakdal99@gmail.com>
2025-01-02 11:00:22 +0900

    refactor : FindPeriodTradeRedDto LocalDateTime (역)직렬화 문제 해결 in redis

--- src/main/java/com/barter/domain/trade/periodtrade/dto/response/FindPeriodTradeResDto.java
+++ src/main/java/com/barter/domain/trade/periodtrade/dto/response/FindPeriodTradeResDto.java
@@ -1,8 +1,15 @@
 package com.barter.domain.trade.periodtrade.dto.response;
 
+import java.time.LocalDateTime;
+
 import com.barter.domain.product.entity.RegisteredProduct;
 import com.barter.domain.trade.enums.TradeStatus;
 import com.barter.domain.trade.periodtrade.entity.PeriodTrade;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,17 +24,26 @@ public class FindPeriodTradeResDto {
 	private String title;
 	private String description;
 	private Long registeredProductId;
-	private String createdAt;
-	private String updatedAt;
+	@JsonSerialize(using = LocalDateTimeSerializer.class) // 직렬화 시 필요
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class) // 역직렬화 시 필요
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	private LocalDateTime createdAt;
+	@JsonSerialize(using = LocalDateTimeSerializer.class) // 직렬화 시 필요
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class) // 역직렬화 시 필요
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	private LocalDateTime updatedAt;
 	private TradeStatus tradeStatus;
 	private int viewCount;
-	private String endedAt;
+	@JsonSerialize(using = LocalDateTimeSerializer.class) // 직렬화 시 필요
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class) // 역직렬화 시 필요
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	private LocalDateTime endedAt;
 	private String address1;
 	private String address2;
 
 	@Builder
 	public FindPeriodTradeResDto(Long periodTradesId, String title, String description, RegisteredProduct product,
-		String createdAt, String updatedAt, TradeStatus tradeStatus, int viewCount, String endedAt
+		LocalDateTime createdAt, LocalDateTime updatedAt, TradeStatus tradeStatus, int viewCount, LocalDateTime endedAt
 		, String address1, String address2) {
 		this.periodTradesId = periodTradesId;
 		this.title = title;
@@ -49,11 +65,11 @@ public class FindPeriodTradeResDto {
 			.title(periodTrade.getTitle())
 			.description(periodTrade.getDescription())
 			.product(periodTrade.getRegisteredProduct())
-			.createdAt(periodTrade.getCreatedAt().toString())
-			.updatedAt(periodTrade.getUpdatedAt().toString())
+			.createdAt(periodTrade.getCreatedAt())
+			.updatedAt(periodTrade.getUpdatedAt())
 			.tradeStatus(periodTrade.getStatus())
 			.viewCount(periodTrade.getViewCount())
-			.endedAt(periodTrade.getEndedAt().toString())
+			.endedAt(periodTrade.getEndedAt())
 			.address1(periodTrade.getAddress1())
 			.address2(periodTrade.getAddress2())
 			.build();
